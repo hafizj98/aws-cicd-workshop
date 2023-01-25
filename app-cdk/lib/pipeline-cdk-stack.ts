3947994e01fa6a53196e554e1e573731839ade67
Hafiz Jamil <hafizjamil265+dev@gmail.com>
2023-01-25 12:10:46 +0000

    Initial Commit

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -2,10 +2,15 @@
 import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
+    const sourceOutput = new codepipeline.Artifact();
+    const unitTestOutput = new codepipeline.Artifact();
 
     const sourceRepo = new codecommit.Repository(this, 'CICD_Workshop', {
       repositoryName: 'CICD_Workshop',
@@ -13,6 +18,44 @@ export class PipelineCdkStack extends Stack {
     });
 
     new CfnOutput(this, 'CodeCommitRepositoryUrl', { value: sourceRepo.repositoryCloneUrlHttp });
+    
+    const pipeline = new codepipeline.Pipeline(this, 'CICD_Pipeline', {
+      pipelineName: 'CICD_Pipeline',
+      crossAccountKeys: false,
+    });
+    
+    const codeQualityBuild = new codebuild.PipelineProject(this, 'Code Quality', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE
+        },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml')
+    });
+    
+    pipeline.addStage({
+      stageName: 'Source',
+        actions: [
+          new codepipeline_actions.CodeCommitSourceAction({
+            actionName: 'CodeCommit',
+            repository: sourceRepo,
+            output: sourceOutput,
+            branch: "main",
+          }),
+        ],
+    });
+
+    pipeline.addStage({
+      stageName: 'Code-Quality-Testing',
+        actions: [
+          new codepipeline_actions.CodeBuildAction({
+            actionName: 'Unit-Test',
+            project: codeQualityBuild,
+            input: sourceOutput,
+            outputs: [unitTestOutput],
+          }),
+        ],
+    });
   }
 }
 
